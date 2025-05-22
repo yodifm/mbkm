85b63eb5803657a1d8fb4cf15fdbdea7050fb916
Surya Nata Ardhana <surya.nata.aardhana@gmail.com>
2025-05-22 20:08:23 +0700

    fix: mbkm to user relation

--- app/Models/Datambkm.php
+++ app/Models/Datambkm.php
@@ -13,6 +13,6 @@ class Datambkm extends Model
 
     public function user()
     {
-        return $this->belongsTo(User::class);
+        return $this->belongsTo(User::class, 'NIM_id', 'NIM');
     }
 }
