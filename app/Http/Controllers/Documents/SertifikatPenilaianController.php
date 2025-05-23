a7112f3de218723da646383e396d1bbff1f2f348
Surya Nata Ardhana <surya.nata.aardhana@gmail.com>
2025-05-23 17:52:17 +0700

    feat: sertifikat & penilaian

---
+++ app/Http/Controllers/Documents/SertifikatPenilaianController.php
@@ -0,0 +1,122 @@
+<?php
+
+namespace App\Http\Controllers\Documents;
+
+use App\Http\Controllers\Controller;
+use App\Models\Documents;
+use Illuminate\Http\Request;
+
+class SertifikatPenilaianController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $title = 'Sertifikat dan Penilaian';
+        $user = auth()->user();
+        $data = Documents::with('user')->where('NIM_id', $user->NIM)->first();
+        $active = 'documents';
+        $subActive = 'sertifikatPenilaian';
+        $titleModal = 'Delete ' . $title;
+        $text = "Are you sure you want to delete?";
+        confirmDelete($titleModal, $text);
+
+
+        $status5 = $data ? $data->status_laporan_akhir : null;
+        $status6 = $data ? $data->status_sertifikat : null;
+        $status7 = $data ? $data->status_penilaian : null;
+        return view('pages.documents.sertifikatPenilaian.index', compact('title', 'data', 'active', 'subActive', 'status5', 'status6', 'status7'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function edit($id)
+    {
+        $title = 'Add Data Sertifikat & Penilaian';
+        $active = 'documents';
+        $data = Documents::find($id);
+        $subActive = 'sertifikatPenilaian';
+        return view('pages.documents.sertifikatPenilaian.create', compact('title', 'data', 'active', 'subActive'));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function update(Request $request, $id)
+    {
+        $credential = $request->validate([
+            'sertifikat' => 'nullable|mimes:pdf|max:2048',
+            'penilaian' => 'nullable|mimes:pdf|max:2048',
+        ]);
+
+        $sertifikatPenilaian = Documents::findOrFail($id);
+
+
+        if ($request->hasFile('sertifikat')) {
+            if (basename($sertifikatPenilaian->file) && file_exists('files/documents/sertifikat/' . basename($sertifikatPenilaian->file))) {
+                unlink('files/documents/sertifikat/' . basename($sertifikatPenilaian->file));
+            }
+
+            $sertifikatPenilaian->status_sertifikat = 'submited';
+            $sertifikatPenilaian->save();
+
+            $file = $request->file('sertifikat');
+            $filename = time() . '.' . $file->getClientOriginalExtension();
+            $file->move('files/documents/sertifikat/', $filename);
+            $url = url('/files/documents/sertifikat/' . $filename);
+            $credential['sertifikat'] = $url;
+        }
+
+        if ($request->hasFile('penilaian')) {
+            if (basename($sertifikatPenilaian->file) && file_exists('files/documents/penilaian/' . basename($sertifikatPenilaian->file))) {
+                unlink('files/documents/penilaian/' . basename($sertifikatPenilaian->file));
+            }
+
+            $sertifikatPenilaian->status_penilaian = 'submited';
+            $sertifikatPenilaian->save();
+
+            $file = $request->file('penilaian');
+            $filename = time() . '.' . $file->getClientOriginalExtension();
+            $file->move('files/documents/penilaian/', $filename);
+            $url = url('/files/documents/penilaian/' . $filename);
+            $credential['penilaian'] = $url;
+        }
+        $sertifikatPenilaian->update($credential);
+
+        return redirect()->route('sertifikat.index')->with('success', 'Sertifikat & Penilaian added successfully');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    // /**
+    //  * Show the form for editing the specified resource.
+    //  */
+    // public function edit(string $id)
+    // {
+    //     //
+    // }
+
+    // /**
+    //  * Update the specified resource in storage.
+    //  */
+    // public function update(Request $request, string $id)
+    // {
+    //     //
+    // }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
---
+++ resources/views/pages/documents/sertifikatPenilaian/index.blade.php
@@ -0,0 +1,72 @@
+@extends('layouts.layout')
+
+@section('section')
+    @include('components.pagetitle', ['title' => $title])
+
+    <div class="card">
+        <div class="card-body">
+            <div class="mb-4 w-100 d-flex justify-content-end">
+                {!! $status5 == 'approved' && ($status6 == 'pending' || $status7 == 'pending')
+                    ? '<a href="' . route('sertifikat.edit', $data->id) . ' " class="btn btn-primary ">Add Documents</a>'
+                    : '' !!}
+
+            </div>
+            <div class="table-responsive">
+                <table class="table table-striped table-bordered">
+                    <thead>
+                        <tr>
+                            <th scope="col">#</th>
+                            <th scope="col">Name</th>
+                            <th scope="col">NIM</th>
+                            <th scope="col">Sertifikat</th>
+                            <th scope="col">Status</th>
+                            <th scope="col">Penilaian</th>
+                            <th scope="col">Status</th>
+                            @if ($data?->status_sertifikat == 'rejected' || $data?->status_penilaian == 'rejected')
+                                <th scope="col">Revisi</th>
+                            @endif
+
+
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @if ($data == null || $data->sertifikat == null || $data->penilaian == null)
+                            <tr>
+                                <td colspan="12" class="text-center">No Data Found.</td>
+                            </tr>
+                        @else
+                            <tr>
+                                <th scope="row">1</th>
+                                <td>{{ $data->user->name }}</td>
+                                <td>{{ $data->user->NIM }}</td>
+                                <td>
+                                    <a href="{{ asset($data->sertifikat) }}" download="{{ basename($data->sertifikat) }}"
+                                        class="btn btn-primary btn-sm">Download Sertifikat</a>
+                                </td>
+                                @include('components.statusFile', [
+                                    'data' => $data->status_sertifikat,
+                                ])
+                                <td>
+                                    <a href="{{ asset($data->penilaian) }}" download="{{ basename($data->penilaian) }}"
+                                        class="btn btn-primary btn-sm">Download Penilaian</a>
+                                </td>
+                                @include('components.statusFile', [
+                                    'data' => $data->status_penilaian,
+                                ])
+                                @if ($data?->status_sertifikat == 'rejected' || $data?->status_penilaian == 'rejected')
+                                    <td>
+                                        @include('components.actionbtn', [
+                                            'edit' => route('sertifikat.edit', $data->id),
+                                            'id' => $data->id,
+                                            'delete' => null,
+                                        ])
+                                    </td>
+                                @endif
+                            </tr>
+                        @endif
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+@endsection
